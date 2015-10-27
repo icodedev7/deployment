dc6b2b49a23d7d11ff6ef90310029121e9e0991d
Amrita pritam <icode.dev7@gmail.com>
2015-10-27 15:41:52 +0530

    commit

---
+++ config/new.rb
@@ -0,0 +1,6 @@
+ShopifyApp::SessionRepository
+$ rails generate shopify_app:install
+
+# or optionally with arguments:
+
+$ rails generate shopify_app:install -api_key=9274b5fb757eea9d28190e00ec188a80 -secret=a42c103cbe0534285b7d2b31c02b364c -redirect_uri=https://customappp.herokuapp.com/
