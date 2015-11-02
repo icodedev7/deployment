7bc4ca5efbabc263fb31d63363f86b03ea0c4da0
amrita pritam <icode.dev7@gmail.com>
2015-11-02 17:36:47 +0530

    commit

--- toolbelt-master/toolbelt.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE stats
-       (id SERIAL PRIMARY KEY,
-        os VARCHAR(255) NOT NULL,
-        user_agent VARCHAR(255),
-        ip VARCHAR(255),
-        referer VARCHAR(255),
-        stamp TIMESTAMP NOT NULL DEFAULT NOW());
-
-CREATE INDEX ts_index ON stats(stamp);
