560cfe62bfedf6cd63e5a3983ba081c11dea72cc
amrita pritam <icode.dev7@gmail.com>
2015-11-02 15:47:36 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
-ShopifyApp::Engine.routes.draw do
-  controller :sessions do
-    get 'login' => :new, :as => :login;
-    post 'login' => :create, :as => :authenticate
-    get 'auth/shopify/callback'=> :callback
-    get 'logout' => :destroy, :as => :logout
-  end 
+Rails.application.routes.draw do
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
 end
