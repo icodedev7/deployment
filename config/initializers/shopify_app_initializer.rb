1c471d83f5b16a4a32b4663a53f6d6211a4fc5d5
amrita pritam <icode.dev7@gmail.com>
2015-11-13 17:47:04 +0530

    commit

--- config/initializers/shopify_app_initializer.rb
+++ config/initializers/shopify_app_initializer.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.api_key = ENV['a0d5ba398f7eb2de0c9dc7672eb94b53']
+  config.api_key = ENV['9274b5fb757eea9d28190e00ec188a80']
   config.secret = ENV['d557fb57a6a882d152545284e02fbc31']
   config.scope = 'read_customers, read_orders, write_products'
   config.embedded_app = true
